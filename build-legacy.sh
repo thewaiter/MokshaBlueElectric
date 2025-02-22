17eec941648829894e4ea086308c65580c54862e
Robert Wiley <ylee@bodhilinux.com>
2025-02-22 09:57:22 -0500

    Normalize gtk theme name

--- build-legacy.sh
+++ build-legacy.sh
@@ -4,6 +4,7 @@
 printf "\n\033[1;33mWARNING:\033[0m For use with Bodhi Linux 7 only.\n\n"
 cd MokshaBlueElectric
 sed -i 's/MokshaBlueElectric/Icons-Moksha-BlueElectric/g' default.edc
+sed -i 's/MokshaBlueElectric/A-Electric-Blue GTK/g' default.edc
 ./build.sh
-sed -i 's/Icons-Moksha-BlueElectric/MokshaBlueElectric/g' default.edc
+sed -i 's/A-Electric-Blue GTK/MokshaBlueElectric/g' default.edc
 cd ..
